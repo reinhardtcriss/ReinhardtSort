7dba536ffff49f65fa8269f1565de71d237ac89c
reinhardtcriss <49617528+reinhardtcriss@users.noreply.github.com>
2019-04-14 12:42:07 -0400

    Update reinhardtsort.java

--- reinhardtsort.java
+++ reinhardtsort.java
@@ -3,6 +3,30 @@
  */
  //wut, i didn't forget how to into a class. Fak u. This is my class. 
  
+ /*
+ ReinhardtSort is an innefficient sorting algorithm. I was asleep in class the day we did bigO notation so 
+ I don't remember how to calculate it so im not gonna, but from my experimentation
+ it would seem that reinhardtsort sorts in exponential time! Without recursion! Wow! Worse than bubblesort! Worse than slowsort?
+How it works:
+
+from the second from the left in the list rightwards, 
+if the number left of the index pointer is greater than the number at the index pointer
+then yeet that sucka to the end of the list. 
+in doing so, you have to shuffle the whole list from the index pointer to the end of the list into the space left of it. this nerfs the 
+number that is larger and left of the index pointer, so it has to be saved in memory so that it can be copied in at the end of the list. 
+Doing this, it requires a LOT of array accesses, but only one integer worth of memory space for stuff, and the pointers and so on. 
+
+this sorting algorithm is not "stable" im pretty sure that it could rearrange numbers of the same value but I could be wrong. it certainly moves them about. 
+
+however it doesn't use that pesky recursion! it is a simple nested for loop arrangement 
+which merely cleverly re-sets its own pointer to make sure that if it completes an operation to check the list over again. 
+this is important because like "bubblesort" it is reliant on list elements working their way towards the beginning in a sort of natural process. 
+I think that technically speaking instead of re-setting the pointer to zero after a completed loop iteration it actually only needs to be re-set to the previous 
+pointer value minus one, which would save a few compare operations each time, but even doing this I think it would still be in exponential time. 
+
+the list is sorted when the pointer reaches the final value without making a change to the list.
+ */
+ 
  
 import java.util.Random; // inna help with make the random number of things.
 import java.util.Arrays; // i nee that for print statement
